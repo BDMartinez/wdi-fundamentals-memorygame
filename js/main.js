c363941b7b548159f23c5a87d6bab8f3fffc580f
strat3gos <BDMartinez@Live.com>
2018-02-05 15:55:04 -0500

    created game logic, version 1.4

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 console.log("Up and running!");
-var cardOne = "Queen";
 
-var cardTwo = "King";
+var cards = ["Queen", "King", "Queen", "King"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
 
-var cardThree = "Queen";
 
-var cardFour = "King";
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
 
 console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
 
-console.log("User flipped " + cardFour);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+
+} 
