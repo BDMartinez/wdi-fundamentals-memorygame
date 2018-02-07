5cb01f773f18d03fd7091052957b0fc491976cf2
strat3gos <BDMartinez@Live.com>
2018-02-07 12:21:08 -0500

    created game logic, version 1.7

--- js/main.js
+++ js/main.js
@@ -29,35 +29,43 @@ var cards = [
 
 	cardImage: 'images/king-of-diamonds.png'
 }
-
 ];
 
 
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
-		} else {
-			alert("Sorry, try again.");
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+				alert("You found a match!");
+			} else {
+				alert("Sorry, try again.");
 		}
+	} 
+};
 
-}
+var flipCard = function() {
+		var cardId = this.getAttribute('data-id')
+		this.setAttribute('src', cards[cardId].cardImage);
 
-var flipCard = function(cardId) {
 		console.log("User flipped " + cards[cardId].rank);
 		cardsInPlay.push(cards[cardId].rank);
 		console.log(cards[cardId].cardImage);
 		console.log(cards[cardId].suit);
+		
 
-	if (cardsInPlay.length === 2) {
 		checkForMatch();
-
-	} 
-
 };
 
-flipCard(0);
-flipCard(1);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
 
+		this.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
