0a4883f054e26debc223e507934474a3efbc449f
strat3gos <BDMartinez@Live.com>
2018-02-06 21:30:56 -0500

    created game logic, verion 1.6

--- js/main.js
+++ js/main.js
@@ -1,6 +1,38 @@
 console.log("Up and running!");
 
-var cards = ["Queen", "King", "Queen", "King"];
+var cards = [
+{
+	rank: 'Queen',
+
+	suit: 'Hearts',
+
+	cardImage: 'images/queen-of-hearts.png',
+}, 
+{
+	rank: 'Queen',
+
+	suit: 'Diamonds',
+
+	cardImage: 'images/queen-of-diamonds.png',
+},
+{
+	rank: 'King',
+
+	suit: 'Hearts',
+
+	cardImage: 'images/king-of-hearts.png',
+}, 
+{
+	rank: 'Kings',
+
+	suit: 'Diamonds',
+
+	cardImage: 'images/king-of-diamonds.png'
+}
+
+];
+
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -14,8 +46,10 @@ var checkForMatch = function() {
 }
 
 var flipCard = function(cardId) {
-		console.log("User flipped " + cards[cardId]);
-		cardsInPlay.push(cards[cardId]);
+		console.log("User flipped " + cards[cardId].rank);
+		cardsInPlay.push(cards[cardId].rank);
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
@@ -26,5 +60,4 @@ var flipCard = function(cardId) {
 
 flipCard(0);
 flipCard(1);
-
 
